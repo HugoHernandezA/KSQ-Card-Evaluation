d20a026bfe66623057891b15f6061b4595337887
HugoHernandezA <hugo.hernandez@theksquaregroup.com>
2023-11-08 23:53:53 -0600

    added db connection pool config and client

---
+++ src/utils/index.js
@@ -0,0 +1,9 @@
+import dbClient from 'pg'
+
+export const dbPool = new dbClient.Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
+});
